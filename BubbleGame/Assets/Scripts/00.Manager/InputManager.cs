f2161cede7cfc2305abc082bb2dc3ecb4e3f191d
Seo Dong Woo <ehddn917@gmail.com>
2025-01-24 22:10:11 +0900

    Input Manager 추가

--- BubbleGame/Assets/Scripts/00.Manager/InputManager.cs
+++ BubbleGame/Assets/Scripts/00.Manager/InputManager.cs
@@ -4,5 +4,16 @@ using Util;
 
 public class InputManager : Singleton<InputManager>
 {
+    public static Vector2 MovePosition => Instance.input.PlayerAction.Move.ReadValue<Vector2>();
+    
     public static Vector2 MousePosition => Mouse.current.position.value;
+
+    private PlayerInputSystem input;
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+        input = new();
+        input.Enable();
+    }
 }
