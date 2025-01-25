c0a85f91feba97146a53b209aff11fd5a9495395
goms7 <36668884+goms7@users.noreply.github.com>
2025-01-25 17:15:41 +0900

    그릇 및 포대 떨어지는 연출
    그릇 및 포대 떨어지는 연출

--- BubbleGame3D/Assets/Scripts/bowl.cs
+++ BubbleGame3D/Assets/Scripts/bowl.cs
@@ -2,49 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.AI;
 public class bowl : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject bowls;
     Sequence BowlSeq;
 
     public Vector3 Base_Pos;
 
     private void Start()
     {
-        init_bowl();
+
+        init_bowl(); 
         shake_bowl();
     }
     public void init_bowl()
     {
         BowlSeq.Kill();
         Debug.Log(Base_Pos);
-        gameObject.transform.DOMove(Base_Pos, 0.1f);
-       // gameObject.transform.position = Base_Pos;
-        //gameObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0,0,-4));
+        gameObject.transform.localPosition = Base_Pos; 
+        gameObject.transform.localRotation = Quaternion.Euler(0, 0, -4);
+
+        //자리 원위치
     }
 
     public void shake_bowl()
     {
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(0, 0, 8), 1f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad));
-        BowlSeq.Join(bowls.transform.DOMoveZ(-0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(0, 0, 8), 1f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad));
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(0, 0, 8), 0.8f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(2f));
-        BowlSeq.Join(bowls.transform.DOMoveZ(-0.03f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(2f));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(0, 0, 8), 0.8f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(2f));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(2f));
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(0, 0, 8), 0.6f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(3.6f));
-        BowlSeq.Join(bowls.transform.DOMoveZ(-0.03f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(4f));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(0, 0, 8), 0.6f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(3.6f));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(4f));
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(0, 0, 8), 0.4f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(4.8f));
-        BowlSeq.Join(bowls.transform.DOMoveZ(-0.04f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(4.8f));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(0, 0, 8), 0.4f).SetRelative(true).SetLoops(2, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(4.8f));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(4.8f));
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(0, 0, 8), 0.3f).SetRelative(true).SetLoops(6, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(5.6f));
-        BowlSeq.Join(bowls.transform.DOMoveZ(-0.04f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(5.6f));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(0, 0, 8), 0.3f).SetRelative(true).SetLoops(6, LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(5.6f));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.01f, 1f).SetRelative(true).SetLoops(2, LoopType.Incremental).SetEase(Ease.OutQuad).SetDelay(5.6f));
 
-        BowlSeq.Append(bowls.transform.DORotate(new Vector3(80, 0, 0), 0.6f).SetRelative(true).SetEase(Ease.OutQuad).SetDelay(7.4f));
-        BowlSeq.Join(bowls.transform.DOMoveY(-1f, 1f).SetRelative(true).SetEase(Ease.InQuad).SetDelay(7.6f));
+        BowlSeq.Append(transform.DOLocalRotate(new Vector3(80, 0, 0), 0.6f).SetRelative(true).SetEase(Ease.OutQuad).SetDelay(7.4f));
+        BowlSeq.Join(transform.DOLocalMoveZ(0.05f, 0.6f).SetRelative(true).SetEase(Ease.OutQuad).SetDelay(7.4f));
+        BowlSeq.Join(transform.DOLocalMoveY(-1f, 1f).SetRelative(true).SetEase(Ease.InQuad).SetDelay(7.6f));
 
     }
 }
