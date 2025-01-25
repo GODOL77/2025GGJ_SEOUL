3efa4c64194e4e889c26b8226f350b1fc76d5266
GODOL <sihyun9346@mokwon.ac.kr>
2025-01-25 21:34:09 +0900

    시작 UI 설정 나가기 추가

--- BubbleGame3D/Assets/Scripts/00.Manager/LobbyManager.cs
+++ BubbleGame3D/Assets/Scripts/00.Manager/LobbyManager.cs
@@ -9,5 +9,15 @@ namespace Manager
         {
             SceneUtil.GoGameScene();
         }
+
+        public void ExitGame()
+        
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit(); // 어플리케이션 종료
+            #endif
+        }
     }
 }
